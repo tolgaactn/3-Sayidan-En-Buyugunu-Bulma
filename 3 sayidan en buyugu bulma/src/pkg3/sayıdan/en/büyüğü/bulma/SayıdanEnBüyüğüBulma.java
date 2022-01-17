064deb5a8b33bbc427b0e816cd6c1442383a00fa
Tolga Ahmet ÇETİN <63610717+tolgaactn@users.noreply.github.com>
2022-01-17 16:57:20 +0300

    Add files via upload

---
+++ 3 sayidan en buyugu bulma/src/pkg3/sayıdan/en/büyüğü/bulma/SayıdanEnBüyüğüBulma.java
@@ -0,0 +1,37 @@
+
+package pkg3.sayıdan.en.büyüğü.bulma;
+
+import java.util.Scanner;
+
+public class SayıdanEnBüyüğüBulma {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Birinci Sayı : ");
+        int a = scanner.nextInt();
+        System.out.println("İkinci Sayı : ");
+        int b = scanner.nextInt();
+        System.out.println("Üçüncü Sayı : ");
+        int c = scanner.nextInt();
+        
+        int maks = -1;
+        
+        if (a >= b && a>= c){
+            
+            maks= a;
+        }
+        else if (b >=a && b>= c ){
+            
+            maks= b;
+        }
+        else {
+            maks = c;
+           
+        }    
+        System.out.println("En büyük sayı : "+maks);
+                
+    }
+    
+}
